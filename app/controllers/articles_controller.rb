632127ac42de869befdc15122738febbcb5fee43
Osama Bin Sajid <osama.sajid@empglabs.com>
2021-08-29 16:39:05 +0500

    update and edit functionality done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,40 @@
 class ArticlesController < ApplicationController
    def show 
-    @article=Article.find(params[:id])
+    @article=Article.find(params[:id]) 
    end 
    def index
       @articles=Article.all
    end
+   def new
+      @article=Article.new
+   end
+   def update
+      @article=Article.find(params[:id])
+      if @article.update(params.require(:article).permit(:title, :description))
+         flash[:notice] = "The article is successfully updated"
+         redirect_to articles_path(@articles)
+      else
+         render "edit"
+      end
+
+
+      
+   end
+   def edit 
+      @article=Article.find(params[:id])
+   end
+   def create
+      @article=Article.new(params.require(:article).permit(:title, :description))
+      if @article.save
+         flash[:notice] = "Article was created successfully"
+         redirect_to articles_path(@article)
+      else
+         render "new"
+      end
+      
+
+      
+   end
+
+   
 end
