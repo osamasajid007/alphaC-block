6d6afe4f67bb7a320abd7c73c1125ee8f570a0f0
Osama Bin Sajid <osama.sajid@empglabs.com>
2021-08-31 09:28:17 +0500

    basic routing done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+   before_action :set_article, only: [:show, :edit, :update, :destroy]
    def show 
-    @article=Article.find(params[:id]) 
+   
    end 
    def index
       @articles=Article.all
@@ -8,9 +9,15 @@ class ArticlesController < ApplicationController
    def new
       @article=Article.new
    end
+   def destroy 
+  
+      @article.destroy
+      flash[:notice] = "The article is deleted"
+      redirect_to articles_path(@article)
+   end
    def update
-      @article=Article.find(params[:id])
-      if @article.update(params.require(:article).permit(:title, :description))
+     
+      if @article.update(article_params)
          flash[:notice] = "The article is successfully updated"
          redirect_to articles_path(@articles)
       else
@@ -21,20 +28,26 @@ class ArticlesController < ApplicationController
       
    end
    def edit 
-      @article=Article.find(params[:id])
+      
    end
    def create
-      @article=Article.new(params.require(:article).permit(:title, :description))
+      @article=Article.new(article_params)
       if @article.save
          flash[:notice] = "Article was created successfully"
          redirect_to articles_path(@article)
       else
          render "new"
       end
-      
-
-      
    end
+   private
+   def set_article
+      @article=Article.find(params[:id])
+   end
+   def article_params
+      params.require(:article).permit(:title, :description)
+   end
+
+
 
    
 end
